e00c8ad79582a975f377b6d846d8b2c2aa734a46
Quixatocs <quixatocs@gmail.com>
2020-12-08 16:52:43 +0000

    generating initial map

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -1,34 +1,53 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class MapGenerator : MonoBehaviour {
+public class MapGenerator : MonoBehaviour 
+{
+    [SerializeField] public Grid grid;
     [SerializeField] private Tile waterTile;
     [SerializeField] private Tile sandTile;
     [SerializeField] private Tile grassTile;
+
+    private Tilemap tileMap;
     
+
+    private void Update() {
+
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // get the collision point of the ray with the z = 0 plane
+            Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+            // get current grid location
+            Vector3Int position = grid.WorldToCell(worldPoint);
+            Debug.Log(position);
+            //Debug.Log(highlightMap.size);
+        }
+    }
+
     void Start()
     {
-        Tilemap tileMap = GetComponent<Tilemap>();
+        tileMap = GetComponent<Tilemap>();
 
         int rows = 10;
         int columns = 10;
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                Tile newTile = ScriptableObject.CreateInstance<Tile>();
-                newTile = sandTile;
-                tileMap.SetTile(new Vector3Int(i, j, 0), newTile);
-            }
-        }
-
+        GenerateStartingMap();
+        
         tileMap.RefreshAllTiles();
         Debug.Log("---");
                
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GenerateStartingMap() 
     {
-        
+        //First set the starting tile to sand
+        tileMap.SetTile(new Vector3Int(0, 0, 0), sandTile);
+        tileMap.SetTile(new Vector3Int(-1, 0, 0), waterTile);
+        tileMap.SetTile(new Vector3Int(-1, 1, 0), waterTile);
+        tileMap.SetTile(new Vector3Int(0, 1, 0), sandTile);
+        tileMap.SetTile(new Vector3Int(1, 0, 0), sandTile);
+        tileMap.SetTile(new Vector3Int(0, -1, 0), sandTile);
+        tileMap.SetTile(new Vector3Int(-1, -1, 0), waterTile);
     }
 }
