84b0cc9cddcb519e837e7b575c2e2695c8c026cc
Quixatocs <quixatocs@gmail.com>
2020-12-09 16:39:46 +0000

    fixed neighbours

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -56,30 +56,69 @@ public class MapGenerator : MonoBehaviour
     private void GenerateTile(Vector3Int newPosition)
     {
         // Get the six neighbours
+        Dictionary<TileBase, int> neighbourWeights = GetNeighbourWeights(newPosition);
         
         //work out the weights of the new tile generation from the neighbours
         
-        tileMap.SetTile(new Vector3Int(0, 0, 0), sandTile);
+        tileMap.SetTile(newPosition, sandTile);
         tileMap.RefreshAllTiles();
     }
 
-    private List<TileBase> GetNeighbours(Vector3Int newPosition)
+    private Dictionary<TileBase, int> GetNeighbourWeights(Vector3Int newPosition)
     {
+        Dictionary<TileBase, int> neighbourWeights = new Dictionary<TileBase, int>();
         List<TileBase> neighbours = new List<TileBase>();
-        TileBase leftNeighbour = tileMap.GetTile(new Vector3Int(newPosition.x - 1, newPosition.y, newPosition.z));
-        TileBase leftUpNeighbour = tileMap.GetTile(new Vector3Int(newPosition.x - 1, newPosition.y + 1, newPosition.z));
-        TileBase rightUpNeighbour = tileMap.GetTile(new Vector3Int(newPosition.x, newPosition.y + 1, newPosition.z));
-        TileBase rightNeighbour = tileMap.GetTile(new Vector3Int(newPosition.x + 1, newPosition.y, newPosition.z));
-        TileBase rightDownNeighbour = tileMap.GetTile(new Vector3Int(newPosition.x, newPosition.y - 1, newPosition.z));
-        TileBase leftDownNeighbour = tileMap.GetTile(new Vector3Int(newPosition.x - 1, newPosition.y - 1, newPosition.z));
-        
-        neighbours.Add(leftNeighbour);
-        neighbours.Add(leftUpNeighbour);
-        neighbours.Add(rightUpNeighbour);
-        neighbours.Add(rightNeighbour);
-        neighbours.Add(rightDownNeighbour);
-        neighbours.Add(leftDownNeighbour);
 
-        return neighbours;
+        TileBase left = tileMap.GetTile(new Vector3Int(newPosition.x - 1, newPosition.y, newPosition.z));
+        
+        TileBase leftUp = newPosition.y % 2 == 0 
+            ? tileMap.GetTile(new Vector3Int(newPosition.x - 1, newPosition.y + 1, newPosition.z))
+            : tileMap.GetTile(new Vector3Int(newPosition.x, newPosition.y + 1, newPosition.z));
+        
+        TileBase rightUp = newPosition.y % 2 == 1 
+            ? tileMap.GetTile(new Vector3Int(newPosition.x + 1, newPosition.y + 1, newPosition.z))
+            : tileMap.GetTile(new Vector3Int(newPosition.x, newPosition.y + 1, newPosition.z));
+        
+        TileBase right = tileMap.GetTile(new Vector3Int(newPosition.x + 1, newPosition.y, newPosition.z));
+        
+        TileBase rightDown = newPosition.y % 2 == 1 
+            ? tileMap.GetTile(new Vector3Int(newPosition.x + 1, newPosition.y - 1, newPosition.z))
+            : tileMap.GetTile(new Vector3Int(newPosition.x, newPosition.y - 1, newPosition.z));
+        
+        TileBase leftDown = newPosition.y % 2 == 0 
+            ? tileMap.GetTile(new Vector3Int(newPosition.x - 1, newPosition.y - 1, newPosition.z))
+            : tileMap.GetTile(new Vector3Int(newPosition.x, newPosition.y - 1, newPosition.z));
+        
+        neighbours.Add(left);
+        neighbours.Add(leftUp);
+        neighbours.Add(rightUp);
+        neighbours.Add(right);
+        neighbours.Add(rightDown);
+        neighbours.Add(leftDown);
+
+        foreach (TileBase neighbour in neighbours)
+        {
+            if (neighbour != null)
+            {
+                if (neighbourWeights.ContainsKey(neighbour))
+                {
+                    neighbourWeights[neighbour] += 1;
+                }
+                else
+                {
+                    neighbourWeights.Add(neighbour, 1);
+                }
+            }
+            
+        }
+        
+        Debug.Log(neighbourWeights.Count);
+        foreach (var neighbourWeight in neighbourWeights)
+        {
+            Debug.Log($"Key: {neighbourWeight.Key}, Val: {neighbourWeight.Value}");
+        }
+        
+
+        return neighbourWeights;
     }
 }
