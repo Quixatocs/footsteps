0ec4af28a06d7d1d337dd534742ef3c7c27b37e6
Quixatocs <quixatocs@gmail.com>
2022-02-08 16:44:25 +0000

    death screen

---
+++ Assets/DeathUIController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathUIController : MonoBehaviour
+{
+    public IntVariable Food;
+    public IntVariable Water;
+    public IntVariable Days;
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(0);
+
+        Food.Value = Food.DefaultValue;
+        Water.Value = Water.DefaultValue;
+        Days.Value = Days.DefaultValue;
+    }
+}
---
+++ Assets/StatTracker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class StatTracker : MonoBehaviour
+{
+    public IntVariable Food;
+    public IntVariable Water;
+    public BoolEvent DeathUIActivationEvent;
+
+    public void CheckFoodAndWater()
+    {
+        if (Food.Value < 0 || Water.Value < 0)
+        {
+            DeathUIActivationEvent.Raise(true);
+        }
+    }
+}
