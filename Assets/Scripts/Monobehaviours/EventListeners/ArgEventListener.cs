823441a9a3b634d72dc9fc3ff7d083bc162c2150
Quixatocs <quixatocs@gmail.com>
2022-02-16 09:33:44 +0000

    Removing more scene asset dependencies

--- Assets/Scripts/Monobehaviours/EventListeners/ArgEventListener.cs
+++ Assets/Scripts/Monobehaviours/EventListeners/ArgEventListener.cs
@@ -1,19 +1,38 @@
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using UnityEngine.Events;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class ArgEventListener<T> : MonoBehaviour
 {
-    public ArgEvent<T> Event;
+    [Header("Asset References")]
+    [SerializeField]
+    private AssetReference EventReference;
+    
+    private ArgEvent<T> @event;
+    
     public UnityEvent<T> Response;
 
     private void OnEnable()
     {
-        Event.RegisterListener(this);
+        Addressables.LoadAssetAsync<ArgEvent<T>>(EventReference).Completed += OnEventAssetLoaded;
+        
     }
 
+    private void OnEventAssetLoaded(AsyncOperationHandle<ArgEvent<T>> obj)
+    {
+        if (obj.Status == AsyncOperationStatus.Succeeded)
+        {
+            @event = obj.Result;
+            Debug.Log($"Successfully loaded asset <{@event.name}>");
+
+            @event.RegisterListener(this);
+        }
+    }
+    
     private void OnDisable()
     {
-        Event.UnregisterListener(this);
+        @event.UnregisterListener(this);
     }
 
     public void OnEventRaised(T arg)
