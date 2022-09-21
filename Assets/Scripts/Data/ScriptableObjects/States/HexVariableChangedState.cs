9d9bdf859b419a4d2f1cc6957bc24f2797502413
Quixatocs <quixatocs@gmail.com>
2022-09-21 12:51:41 +0100

    #10 removing old states

--- Assets/Scripts/Data/ScriptableObjects/States/HexVariableChangedState.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/States/HexVariableChangedState", order = 1)]
-[Serializable]
-public class HexVariableChangedState : VariableChangedState<HexVariable>
-{
-}
--- Assets/Scripts/Data/ScriptableObjects/States/IntVariableChangedState.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/States/IntVariableChangedState", order = 1)]
-[Serializable]
-public class IntVariableChangedState : VariableChangedState<IntVariable>
-{
-}
