3bc4b8a3beb412425ae8e5a06605f135f200ba63
Quixatocs <quixatocs@gmail.com>
2022-10-05 16:29:57 +0100

    wave function collapse initial implementation #11

--- Assets/Scripts/Data/ScriptableObjects/Functions/WorldGenerationAlgorithms/WaveFunctionCollapseWorldGenerationAlgorithm.cs
+++ Assets/Scripts/Data/ScriptableObjects/Functions/WorldGenerationAlgorithms/WaveFunctionCollapseWorldGenerationAlgorithm.cs
@@ -7,6 +7,31 @@ public class WaveFunctionCollapseWorldGenerationAlgorithm : WorldGenerationAlgor
 {
     public override WorldTile GenerateTile(ReadOnlyCollection<WorldTile> allWorldTiles, List<WorldTile> currentTileExistingNeighbours = null)
     {
-        throw new System.NotImplementedException();
+
+        List<string> allTileNeighbourConnections = new List<string>();
+
+        foreach (WorldTile neighbourWorldTile in currentTileExistingNeighbours)
+        {
+            foreach (TileNeighbourInfo tileNeighbourInfo in neighbourWorldTile.tileNeighbourInfos)
+            {
+                allTileNeighbourConnections.Add(tileNeighbourInfo.tileName);
+            }
+        }
+        
+        int rngRandomWorldTile = Random.Range(0, allWorldTiles.Count);
+        
+        if (allTileNeighbourConnections.Count == 0) return allWorldTiles[rngRandomWorldTile].Copy();
+        
+        int rng = Random.Range(0, allTileNeighbourConnections.Count);
+        
+        foreach (WorldTile foundWorldTile in allWorldTiles)
+        {
+            if (foundWorldTile.tileName == allTileNeighbourConnections[rng])
+            {
+                return foundWorldTile.Copy();
+            }
+        }
+            
+        return allWorldTiles[rngRandomWorldTile].Copy();
     }
 }
